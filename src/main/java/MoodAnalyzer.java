0c739ec66147d770cce068a2cffa12c463b4e8da
vishakhakshirsagar <kshirsagarvishakha22@gmail.com>
2023-01-07 18:27:33 +0530

    Added: Handle Exceptions if user provide Invalid Mood

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,9 +1,19 @@
 public class MoodAnalyzer {
-        public String analyzeMood(String message) {
+    String message;
 
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyzeMood(String message) {
+
+        try {
             if (message.contains("SAD"))
                 return "SAD";
             else
                 return "HAPPY";
+        } catch (NullPointerException e) {
+            return "HAPPY";
         }
     }
+}
